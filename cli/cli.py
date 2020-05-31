de016495f3e8a09afbae95a653b35cc4eaf103cf
conor <conormurphyx@gmail.com>
2020-05-30 23:21:34 -0400

    added simple ping pong tcp client and server

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,13 @@
+from multiprocessing import Process
+
+from networking import tcpClient
+from networking.tcpServer import TcpServer
+
 
 def main():
-    pass
+    server = TcpServer(6719)
+    server.run()
+    tcpClient.send_packet("hello", '127.0.0.1', 6719)
 
 
 if __name__ == "__main__":
