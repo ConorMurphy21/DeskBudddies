903afca7be0809116c60fa5ff4f4c4fd1cbc668f
conor <conormurphyx@gmail.com>
2020-05-31 23:50:38 -0400

    changed packet structure

--- networking/packets/packet.py
+++ networking/packets/packet.py
@@ -1,12 +1,48 @@
 
+# Packet structure:
+# the first 4 bytes will be the length of the packet
+# the next byte will be the packet identifier
+# the remaining bytes will be json string representing a dictionary of values
+import json
+import struct
+
+from cli.action import Action
+
+
 class Packet:
+    # required fields for each action
+    REQS = {Action.QUERY: ['uid', 'date'],
+            Action.GET: ['date', 'week'],
+            Action.REMOVE: ['uid', 'date']}
 
-    def __init__(self):
-        self.eos = True
+    def __init__(self, packet_id: str, data: dict):
+        self.packet_id = packet_id
+        self.data = data
 
-    def bytes(self):
-        pass
+    def encode(self):
+        # the packet id header
+        data = self.packet_id.encode('utf-8')
+        data += json.dumps(self.data).encode('utf-8')
+        # prepend the length to the front of the message
+        msg = struct.pack('>I', len(data)) + data
+        return msg
 
 
-def from_str(stream: str):
-    print(str)
+# removes the first 5 bytes because we don't need to except long messages on the server side
+def from_server_str(stream: str) -> Packet:
+    stream = stream[4:len(stream)]
+    return from_str(stream)
+
+
+def from_str(stream: str) -> Packet:
+    packet_id = stream[0]
+    data = json.loads(stream[1:len(stream)])
+    return Packet(packet_id, data)
+
+
+def from_args(args) -> Packet:
+    packet_id = str(int(args.action))
+    data = {}
+    for key in Packet.REQS[args.action]:
+        data[key] = args[key]
+    return Packet(packet_id, data)
---
+++ server/serverQueryManager.py
@@ -0,0 +1,14 @@
+
+# These are all of the methods callable by the client, that interact with the server
+
+
+def add(args: dict) -> dict:
+    pass
+
+
+def remove(args: dict) -> dict:
+    pass
+
+
+def get(args: dict) -> dict:
+    pass
