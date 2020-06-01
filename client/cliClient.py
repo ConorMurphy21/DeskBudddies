e7b3c27e46bcb4c89120eeda88e065efd5dceede
conor <conormurphyx@gmail.com>
2020-06-01 00:03:28 -0400

    continued fixing up networking on client side

--- client/cliClient.py
+++ client/cliClient.py
@@ -1,20 +1,23 @@
 from cli.action import Action
 from client.clientConfig import ClientConfig
 from cmnUtils import directoryFinder
+from networking import tcpClient
+from networking.packets import packet as pct
+from networking.packets.packet import Packet
 
 
 def main(args):
     settings = ClientConfig(directoryFinder.client_config_file())
     if args.action == Action.CONFIG:
-        config(settings)
+        _config(settings)
         return
 
     if settings['host'] == '' or settings['ip'] == '':
-        required_config(settings)
-    ask_server(args, settings)
+        _required_config(settings)
+    _ask_server(args, settings)
 
 
-def required_config(settings):
+def _required_config(settings):
     print("It looks like this is your first time using it.")
     host = input("Please enter the hostname or ip of your DeskBuddies server: ")
     uid = input("Please enter your user id: ")
@@ -22,9 +25,17 @@ def required_config(settings):
     settings['uid'] = uid
 
 
-def config(settings):
+def _config(settings):
     pass
 
 
-def ask_server(args, settings):
+def _ask_server(args, settings):
+    # just in case any required info for the packet creation is configured (like uid)
+    args += settings
+    packet = pct.from_args(args)
+    response = tcpClient.send_packet(packet, settings['host'], settings['port'])
+    print(_parse_response(response))
+
+
+def _parse_response(response: Packet) -> str:
     pass
