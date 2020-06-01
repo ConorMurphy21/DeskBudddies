a9b022e2f063aa092da3aec4dcf99915d8f501e5
conor <conormurphyx@gmail.com>
2020-05-31 22:22:41 -0400

    setup cli Client basic method, and fixed config structure

--- client/clientConfig.py
+++ client/clientConfig.py
@@ -4,17 +4,8 @@ from cmnUtils.simpleConfig import SimpleConfig
 
 class ClientConfig(SimpleConfig):
 
-    # this object acts as a data class, that can be converted to and from JSON easily for storage
-    # any complex objects that need to be stored, can be stored via links to other files
-    # and an override of these properties get and set functionality
-    def __init__(self):
-        super().__init__()
-        self.serverIp = None
-        self.uid = None
-        self.port = 6719
-
-    def from_json(self, json_str: str):
-        self.props = simpleConfig.to_dict(json_str)
-        self.serverIp = self.props['serverIp']
-        self.uid = self.props['uid']
-        self.port = self.props['port']
+    def set_default_data(self):
+        self.data = {
+            'host': '',
+            'uid': '',
+        }
