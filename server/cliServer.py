a7d0b65ec9ab8ed37d48f8fd9ae2853ffc140aa3
Jen <45193617+jengu288@users.noreply.github.com>
2020-06-01 23:37:43 -0700

    requested changes and writing to csv

--- server/cliServer.py
+++ server/cliServer.py
@@ -1,3 +1,23 @@
+import os
+
+from cmnUtils import directoryFinder
+from cmnUtils.configManager import user_config_interface
+from server.adjacencyMatrix import AdjacencyMatrix
+from server.serverConfig import ServerConfig
+
 
 def main(args):
-    pass
+    settings = ServerConfig(directoryFinder.server_config_file())
+    if  not os.path.isfile("ServerAdjacencyMatrix.csv"):
+        csv = input("An adjacency matrix from the csv has not been made, please provide an appropriate csv:")
+        while True:
+            try:
+                adj = AdjacencyMatrix(csv)
+                print(adj)
+                break
+            except FileNotFoundError:
+                csv = input("The csv could not be opened, try again:")
+
+    print("Unfortunately, we don't support querying the server because Python hates multithreading, so get all your "
+          "configuration done now")
+    user_config_interface(settings)
--- server/serverConfig.py
+++ server/serverConfig.py
@@ -3,19 +3,8 @@ from cmnUtils.simpleConfig import SimpleConfig
 
 
 class ServerConfig(SimpleConfig):
+    def set_default_data(self):
+        self.data = {
+            'port': 6719
+        }
 
-    # this object acts as a data class, that can be converted to and from JSON easily for storage
-    # any complex objects that need to be stored, can be stored via links to other files
-    # and an override of these properties get and set functionality
-    def __init__(self):
-        super().__init__()
-        self.adjacencyFile = None
-        self.scheduleFile = None
-        self.port = 6719
-
-
-def from_json(self, json_str: str):
-    self.props = simpleConfig.to_dict(json_str)
-    self.adjacencyFile = self.props['adjacencyFile']
-    self.scheduleFile = self.props['scheduleFile']
-    self.port = self.props['port']
