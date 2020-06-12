728dbae39892e51cc61f7bfafb3f4cd7b9f25505
conor <conormurphyx@gmail.com>
2020-06-12 02:03:57 -0400

    reformatted dateUtils

--- cmnUtils/dateUtils.py
+++ cmnUtils/dateUtils.py
@@ -7,5 +7,6 @@ def get_next_weekday(weekday: int) -> datetime:
     # make sure to zero the time
     return datetime.datetime(weekday.year, weekday.month, weekday.day)
 
+
 def string_to_datetime(string_date) -> datetime:
     return datetime.datetime.strptime(string_date, '%Y%m%d')
