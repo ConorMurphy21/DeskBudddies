6858ce508ee1592b80b591db6686ba96ace2646b
Dakota Crozier <dakotac@sfu.ca>
2020-06-14 14:59:54 -0700

    added get_week function

--- cmnUtils/dateUtils.py
+++ cmnUtils/dateUtils.py
@@ -10,3 +10,16 @@ def get_next_weekday(weekday: int) -> datetime:
 
 def string_to_datetime(string_date) -> datetime:
     return datetime.datetime.strptime(string_date, '%Y%m%d')
+
+
+def get_week(weekday: datetime, mondayfirstday: bool) -> datetime:
+    if mondayfirstday:
+        weekday -= datetime.timedelta(days=weekday.weekday())
+    else:
+        day = weekday.weekday()
+        if day <= 5:
+            day += 1
+        else:
+            day = 0
+        weekday -= datetime.timedelta(days=day)
+    return datetime.datetime(weekday.year, weekday.month, weekday.day)
