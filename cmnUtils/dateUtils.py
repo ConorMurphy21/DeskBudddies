66cae6547da8f8db42c9b7042653c905c67342a7
Dakota Crozier <dakotac@sfu.ca>
2020-06-14 23:08:57 -0700

    dateUtils.py bug fix

--- cmnUtils/dateUtils.py
+++ cmnUtils/dateUtils.py
@@ -28,12 +28,12 @@ def validate_date_format(format_str: str) -> bool:
     #  tests date string format with 2 test dates to ensure sufficient data is kept from parsing
     date1 = datetime.datetime(2018, 11, 7)
     date2 = datetime.datetime(2019, 4, 1)
-    result = _date_test(date1, format_str) + _date_test(date2, format_str)
-    if result % 2 == 0:
-        return True
-    else:
-        print("Date format doesn't keep track of day")
+    result = _date_test(date1, format_str)
+    result2 = _date_test(date2, format_str)
+    if result == 0 or result2 == 0:
         return False
+    else:
+        return True
 
 
 def _date_test(test_datetime: datetime, format_str: str) -> int:
@@ -48,16 +48,16 @@ def _date_test(test_datetime: datetime, format_str: str) -> int:
     month_new = test_datetime_new.month
     day_new = test_datetime_new.day
     if day != day_new:
-        return 1
+        return 0
     if year != year_new:
         if month != month_new:
-            return 2
+            return 1
         else:
-            return 4
+            return 2
     elif month == month_new:
-        return 6
+        return 3
     else:
-        return 1
+        return 0
 
 
 def parse_date_str(date_str: str, format_str: str) -> datetime:
@@ -68,8 +68,9 @@ def parse_date_str(date_str: str, format_str: str) -> datetime:
     #   move it back one day
     tomorrow_datetime = datetime.date(tomorrow_datetime.year, tomorrow_datetime.month, tomorrow_datetime.day - 1)
     type_date_format = _date_test(parsed_date, format_str)
-
-    if type_date_format == 2:  # only day is given
+    if type_date_format == 0:
+        raise ValueError()
+    if type_date_format == 1:  # only day is given
         parsed_date_zeroed = datetime.date(parsed_date)
         days_to_add = abs(current_day.day - parsed_date_zeroed().day)
         if parsed_date_zeroed == current_day:
@@ -78,7 +79,7 @@ def parse_date_str(date_str: str, format_str: str) -> datetime:
             fixed_parsed_date = current_day + current_day.timedelta(days=days_to_add)
         else:
             fixed_parsed_date = current_day - current_day.timedelta(days=days_to_add)
-    if type_date_format == 4:  # only day month is given
+    if type_date_format == 2:  # only day month is given
         parsed_date = parsed_date.date().replace(year=current_day.year)
         if parsed_date >= current_day:
             # if greater then go back one year
@@ -86,7 +87,9 @@ def parse_date_str(date_str: str, format_str: str) -> datetime:
             fixed_parsed_date = datetime.datetime(parsed_date.year, parsed_date.month, parsed_date.day)
     else:
         fixed_parsed_date = datetime.datetime(parsed_date.year, parsed_date.month, parsed_date.day)
-    if parsed_date < datetime.datetime.now():
+    true_date = datetime.datetime.now()
+    true_date = datetime.datetime(true_date.year, true_date.month, true_date.day)
+    if parsed_date < true_date:
         print("day has already passed")
-        #throw error
+        raise ValueError()
     return fixed_parsed_date
